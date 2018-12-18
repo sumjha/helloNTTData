6ee86b685ffdff0723cc3301ea4580028f3cacfc
sumjha <42369600+sumjha@users.noreply.github.com>
2018-12-18 16:31:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/localhost/htdocs"}
 LOG=${LOG:-"/var/log/darkhttpd/access.log"}
 
 #/usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 80 --no-server-id
-export LD_LIBRARY_PATH = $LD_LIBRARY_PATH:.
 java -jar MyHttpServer.jar -Djava.library.path=.
