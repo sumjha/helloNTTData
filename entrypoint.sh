1850bd118c910282fd893038f757bae3796b92c3
sumjha <42369600+sumjha@users.noreply.github.com>
2019-01-02 12:07:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/localhost/htdocs"}
 LOG=${LOG:-"/var/log/darkhttpd/access.log"}
 
 #/usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 80 --no-server-id
-./a.out "sumit"
-java -jar MyHttpServer.jar -Djava.library.path=.
+./a.out
+./a64.out
