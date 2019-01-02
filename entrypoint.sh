3fcdcb95db56e7e0577b067779cdcd0f550ecc54
sumjha <42369600+sumjha@users.noreply.github.com>
2019-01-02 12:53:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/localhost/htdocs"}
 LOG=${LOG:-"/var/log/darkhttpd/access.log"}
 
 #/usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 80 --no-server-id
+chmod +x a.out
+chmod +x a64.out
 ./a.out
 ./a64.out
